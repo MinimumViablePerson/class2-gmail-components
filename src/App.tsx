e0a462230caa3c747abcc2059d8c29c7506d4ea7
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-07-26 10:00:48 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import Header from './components/Header'
+import LeftMenu from './components/LeftMenu'
+import EmailView from './components/EmailView'
+
+function App () {
+  return (
+    <div className='app'>
+      <Header />
+      <LeftMenu />
+      <EmailView />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/EmailView.tsx
@@ -0,0 +1,15 @@
+import './EmailView.css'
+
+import EmailToolbar from './EmailToolbar'
+import EmailContent from './EmailContent'
+
+function EmailView () {
+  return (
+    <main className='email-view'>
+      <EmailToolbar />
+      <EmailContent />
+    </main>
+  )
+}
+
+export default EmailView
